557c100d6f8d8e7b6d8bdc5bddb3c88d16276264
Ryan Harrison <mail@ryanharrison.co.uk>
2019-08-31 21:58:21 +0100

    Run load test through Jest

---
+++ tests/load-homepage.test.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer");
+const { measurePageLoad } = require("../measure/measurePageLoad");
+
+let browser;
+
+beforeAll(async () => {
+    console.log("Closing browser");
+    browser = await puppeteer.launch();
+});
+
+afterAll(async () => {
+    console.log("Closing browser");
+    if (browser && browser.isConnected()) await browser.close();
+});
+
+describe("Load Homepage", async () => {
+    let runData;
+    beforeAll(async () => {
+        const url = "https://ryanharrison.co.uk";
+        runData = await measurePageLoad(browser, "load-homepage", url);
+    });
+
+    test("full page load should be within 1 second", () => {
+        expect(runData.timings.fullTime).toBeLessThan(1000);
+    });
+
+    it("should have less than 1500 nodes", () => {
+        expect(runData.metrics.nodes).toBeLessThan(1500);
+    });
+});
